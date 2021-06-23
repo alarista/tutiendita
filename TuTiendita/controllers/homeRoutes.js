ad17df72e7c9be9733ea822e19f8b5a7b2cab77c
alarista <alsynoe@gmail.com>
2021-06-23 18:23:00 -0500

    Adding files so I can be up to date

--- TuTiendita/controllers/homeRoutes.js
+++ TuTiendita/controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Example } = require('../models');
+const { Example, Product } = require('../models');
 
 router.get('/', async (req, res) => {
   try {
@@ -17,5 +17,29 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.get('/products', async(req,res)=>{
+    try {
+      const productsList = await Product.map({
+        include: [
+          {
+            model: Product,
+            attributes: [
+              'product_name',
+              'price',
+              'category',
+              'filename',
+              'description',
+            ],
+          },
+        ],
+      });
+      const products = productsList.get({ plain: true });
+      res.render('products-seller', { products });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  })
 
 module.exports = router;
