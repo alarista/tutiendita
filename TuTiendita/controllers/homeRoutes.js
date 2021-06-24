c6db532d8d496f9d80f28d1782d1a2141fecf5dc
Erick Contreras <Erickcc06@gmail.com>
2021-06-24 17:56:10 -0600

    Added login to homeroute

--- TuTiendita/controllers/homeRoutes.js
+++ TuTiendita/controllers/homeRoutes.js
@@ -22,18 +22,13 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/products', async(req,res)=>{
-    try {
-      const productsList = await Product.findAll();
-      const products = productsList.map((product) => 
-        product.get({plain: true}));
-        
-      res.render('products-seller', { products });
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  })
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
+  res.render('login');
+});
 
 module.exports = router;
