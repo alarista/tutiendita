e159a3263cc50086dcc10100ab3fd8fbcdd5fb86
00anp <alfonso.nunez29@gmail.com>
2021-06-24 19:18:22 -0500

    updated routes

--- TuTiendita/controllers/index.js
+++ TuTiendita/controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const yourStoreRoutes = require('./yourStoreRoutes');
+const ordersRoutes = require('./orders-routes');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/your_store', yourStoreRoutes);
+router.use('/orders', ordersRoutes);
 
 module.exports = router;
