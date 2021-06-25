7745e902c635ad1f587abcbf6f0c192befc001da
Erick Contreras <Erickcc06@gmail.com>
2021-06-25 03:42:57 -0600

    Upadated store route

--- TuTiendita/controllers/store-routes.js
+++ TuTiendita/controllers/store-routes.js
@@ -5,14 +5,62 @@ const withAuth = require('../utils/auth');
 
 router.get('/:id', async (req, res) => {
     try {
-      const products = await Product.findAll({
-        attributes: ['product_name', 'price', 'category', 'stock', 'filename'],
+      const dairy = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
         where: {
-          storeOwner_id: req.params.id,
+          category: "Dairy",
         },
         raw: true
       });
-      res.render('store', { products, loggedIn: req.session.loggedIn });
+
+      const pantry = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Pantry",
+        },
+        raw: true
+      });
+
+      const fruitsAndVegetales = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Fruits and Vegetables",
+        },
+        raw: true
+      });
+
+      const veberages = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Veberages",
+        },
+        raw: true
+      });
+
+      const bread = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Bread",
+        },
+        raw: true
+      });
+
+      const delicatessen = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Delicatessen",
+        },
+        raw: true
+      });
+
+      const snacksAndCandy = await Product.findAll({
+        attributes: ['product_name', 'price', 'stock', 'filename'],
+        where: {
+          category: "Snacks and Candy",
+        },
+        raw: true
+      });
+      res.render('store', { dairy, pantry, fruitsAndVegetales, veberages, bread, delicatessen, snacksAndCandy, loggedIn: req.session.loggedIn });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
