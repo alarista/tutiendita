2501182d0c1fe679a9678f8052cba3e6163b9af1
Erick Contreras <Erickcc06@gmail.com>
2021-06-24 19:43:47 -0600

    Added another query to the same route and sent it to product-seller handlebar

--- TuTiendita/controllers/yourStoreRoutes.js
+++ TuTiendita/controllers/yourStoreRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Product, StoreOwner } = require('../models');
+const sequelize = require('../config/connection');
 const withAuth = require('../utils/auth');
 
 router.get('/:id', async (req, res) => {
@@ -11,7 +12,21 @@ router.get('/:id', async (req, res) => {
         },           
       });
       const storeOwner = storeOwnerData.get({ plain: true });
-      res.render('products-seller', { storeOwner, loggedIn: req.session.loggedIn });
+
+      const query = "SELECT o.id, o.status, s.store_name, c.address1, p.product_name, p.price, od.quantity, od.total, c.address2, c.city, c.state, c.postal_code, c.phone " +
+      "FROM storeOwner s "+
+      "INNER JOIN orders o "+
+      "ON s.id = o.store_owner_id "+
+      "INNER JOIN customers c "+
+      "ON o.customer_id = c.id "+
+      "INNER JOIN order_details od "+
+      "ON o.id = od.order_id "+
+      "INNER JOIN product p "+
+      "ON od.product_id = p.id "+
+      "ORDER BY od.order_id ASC"
+      const [ordersData, metadata] = await sequelize.query(query);
+
+      res.render('products-seller', { storeOwner, ordersData, loggedIn: req.session.loggedIn });
       // res.send(storeOwner).json()
     } catch (err) {
       console.log(err);
