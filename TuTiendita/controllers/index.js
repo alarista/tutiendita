47b12d5dbdf2d796a41bf157c2fb4a753b12c64c
Erick Contreras <Erickcc06@gmail.com>
2021-06-25 19:01:17 -0600

    Added orders routes

--- TuTiendita/controllers/index.js
+++ TuTiendita/controllers/index.js
@@ -3,10 +3,12 @@ const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const yourStoreRoutes = require('./yourStoreRoutes');
 const storeRoutes = require('./store-routes');
+const ordersRoutes = require('./orders-history-routes');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 router.use('/your_store', yourStoreRoutes);
 router.use('/store', storeRoutes);
+router.use('/orders', ordersRoutes);
 
 module.exports = router;
