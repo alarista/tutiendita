9a0653b3bb957fe7d6126652d4959b0fbc6350ac
00anp <alfonso.nunez29@gmail.com>
2021-06-25 19:37:00 -0500

    storeowner.js

--- TuTiendita/public/js/storeownerview.js
+++ TuTiendita/public/js/storeownerview.js
@@ -1,79 +1,118 @@
-
-var deleteProductButtons = document.getElementsByClassName('btn-danger')
-for (var i = 0; i < deleteProductButtons.length; i++) {
-    var button = deleteProductButtons[i]
-    button.addEventListener('click', removeProduct)
-}
+var cardEl = document.querySelector('#container4');
 
 function removeProduct(event) {
-    var buttonClicked = event.target
-    buttonClicked.parentElement.parentElement.remove()
+  var buttonClicked = event.target;
+  var textEl = event.target.textContent;
+  console.log(textEl);
+  if(textEl === 'Erase it'){
+    // console.log('Clicked a card');
+    var productName = event.target.parentElement.firstElementChild.textContent.trim();
+    // console.log("Product name:", productName);
+    buttonClicked.parentElement.parentElement.remove();
+  } 
 }
 
-var seeOrders = document.getElementById("see_orders")
-var seeProducts = document.getElementById("see_products")
-var addProduct = document.getElementById("add_product")
-var storeProducts = document.getElementById("store_products")
-var storeOrders = document.getElementById("store_orders")
-var productForm = document.getElementById("product_form")
-
+var seeOrders = document.getElementById("see_orders");
+var seeProducts = document.getElementById("see_products");
+var addProduct = document.getElementById("add_product");
+var storeProducts = document.getElementById("store_products");
+var storeOrders = document.getElementById("store_orders");
+var productForm = document.getElementById("product_form");
 
 seeOrders.addEventListener("click", showOrders);
-function showOrders(){
-    storeProducts.style.display = "none";
-    seeOrders.classList.add("active");
-    addProduct.classList.remove("active");
-    seeProducts.classList.remove("active");
-    storeOrders.style.display = "block";
-    productForm.style.display = "none";
+function showOrders() {
+  storeProducts.style.display = "none";
+  seeOrders.classList.add("active");
+  addProduct.classList.remove("active");
+  seeProducts.classList.remove("active");
+  storeOrders.style.display = "block";
+  productForm.style.display = "none";
 }
 
 seeProducts.addEventListener("click", showProducts);
-function showProducts(){
-    storeProducts.style.display = "block";
-    seeOrders.classList.remove("active");
-    addProduct.classList.remove("active");
-    seeProducts.classList.add("active");
-    storeOrders.style.display = "none";
-    productForm.style.display = "none";
+function showProducts() {
+  storeProducts.style.display = "block";
+  seeOrders.classList.remove("active");
+  addProduct.classList.remove("active");
+  seeProducts.classList.add("active");
+  storeOrders.style.display = "none";
+  productForm.style.display = "none";
 }
 
 addProduct.addEventListener("click", addProducts);
-function addProducts(){
-    storeProducts.style.display = "none";
-    storeOrders.style.display = "none";
-    productForm.style.display = "block";
-    seeOrders.classList.remove("active");
-    seeProducts.classList.remove("active");
-    addProduct.classList.add("active");
+function addProducts() {
+  storeProducts.style.display = "none";
+  storeOrders.style.display = "none";
+  productForm.style.display = "block";
+  seeOrders.classList.remove("active");
+  seeProducts.classList.remove("active");
+  addProduct.classList.add("active");
 }
 
-var registerNewProduct = document.getElementById("done_button")
+var registerNewProduct = document.getElementById("done_button");
 
 const ProductFormHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const productTitle = document.querySelector('#new_product_name').value.trim();
-    const productPrice = document.querySelector('#new_product_price').value.trim();
-    const productStock = document.querySelector('#new_product_stock').value.trim();
-    const productCategory = document.querySelector('#inputState').value.trim();
-    const productImg = document.querySelector('#new_product_img').value.trim();
-    console.log(productTitle);
+  const productTitle = document.querySelector("#new_product_name").value.trim();
+  const productPrice = document
+    .querySelector("#new_product_price")
+    .value.trim();
+  const productStock = document
+    .querySelector("#new_product_stock")
+    .value.trim();
+  const productCategory = document.querySelector("#inputState").value.trim();
+  const productImg = document.querySelector("#new_product_img").value.trim();
+  console.log(productTitle);
 
-  
-    if (productTitle && productPrice && productStock && productCategory && productImg) {
-      const response = await fetch('/api/stores/1', {
-        method: 'POST',
-        body: JSON.stringify({ productTitle, productPrice, productStock, productCategory, productImg }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+  if (
+    productTitle &&
+    productPrice &&
+    productStock &&
+    productCategory &&
+    productImg
+  ) {
+    const response = await fetch("/api/stores/1", {
+      method: "POST",
+      body: JSON.stringify({
+        productTitle,
+        productPrice,
+        productStock,
+        productCategory,
+        productImg,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
 
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Failed to add product.');
-      }
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add product.");
     }
-  };
-  
-  registerNewProduct.addEventListener("click", ProductFormHandler);
+  }
+};
+
+const ProductFormDeleter = async (event) => {
+  event.preventDefault();
+
+  const findURL = removeProduct();
+  // console.log(findURL);
+
+  if (findURL) {
+    const response = await fetch("/api/products/" + findURL, {
+      method: "DELETE",
+      body: JSON.stringify(),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Failed to add product.");
+    }
+  }
+};
+
+deleteProductButtons.addEventListener("click", ProductFormDeleter);
+
+registerNewProduct.addEventListener("click", ProductFormHandler);
